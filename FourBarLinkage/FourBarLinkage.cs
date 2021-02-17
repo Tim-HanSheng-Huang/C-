e048213758cc6d25e42d8f33913c93a2601bc408
HanSheng, Huang <78677864+Tim-HanSheng-Huang@users.noreply.github.com>
2021-02-17 18:50:43 +0800

    Add files via upload

---
+++ FourBarLinkage/FourBarLinkage.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R09725060HSHunagAss11
+{
+    class FourBarLinkage
+    {
+        double scale;
+        int xoff, yoff;
+
+        public Point PointOfInterest
+        {
+            get { return TransformPoint(Pp); }
+        }
+
+        public void DrawLinkage(Graphics g, Rectangle bound)
+        {
+            // Update xoffset, yoffset and scale with respect to the rectangle
+            scale = bound.Width / (Lc + Lf + Lg) * 1.1;
+            xoff = (int)(Ld * scale * 2.0f);
+            yoff = (int)(bound.Height / 3.0f);
+            // set screen points
+            Point s1, s2, sc, sf, sp;
+            s1 = TransformPoint(P1);
+            s2 = TransformPoint(P2); 
+            sc = TransformPoint(Pc); 
+            sf = TransformPoint(Pf); 
+            sp = TransformPoint(Pp);
+            // Draw 6 lines
+            float penWidth = bound.Height / 40;
+            Pen myPen = new Pen(Color.Green, penWidth);
+            myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            myPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            g.DrawLine(myPen, s1, sc);
+            myPen.Color = Color.Red;
+            g.DrawLine(myPen, s1, s2);
+            myPen.Color = Color.Yellow;
+            g.DrawLine(myPen, s2, sf);
+            myPen.Color = Color.Blue;
+            g.DrawLine(myPen, sc, sf);
+            g.DrawLine(myPen, sc, sp);
+            g.DrawLine(myPen, sp, sf);
+
+
+        }
+
+        Point TransformPoint(PointF pt)
+        {
+            Point p = Point.Empty;
+            p.X = (int)(xoff + pt.X * scale);
+            p.Y = (int)(yoff + pt.Y * scale);
+            return p;
+        }
+
+        double Lg = 20.0;
+        double Ld = 10.0;
+        double Lc = 35.0;
+        double Lf = 25.0;
+        double Da = 17.0;
+        double Db = 5.0;
+        double alpha = 1.0;
+        string warnning = "";
+
+        public string Warnning
+        {
+            get => warnning;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Angle={alpha:0.00},P1=({P1.X},{P1.Y}),P2=({P2.X},{P2.Y}),Pc=({Pc.X:0.00},{Pc.Y:0.00}),Pf=({Pf.X:0.00},{Pf.Y:0.00}),Pp=({Pp.X:0.00},{Pp.Y:0.00})";
+        }
+
+        public FourBarLinkage()
+        {
+            updateConfiguration(alpha);
+        }
+
+
+        PointF P1=new PointF(0,0), P2, Pc, Pf, Pp;
+
+        [Category("連桿結構")]
+        public double POIAxialOffset
+        {
+            get => Da;
+            set
+            {
+                Da = value;
+            }
+        }
+
+        [Category("連桿結構")]
+        public double POIRadialOffset
+        {
+            get => Db;
+            set
+            {
+                Db = value;
+            }
+        }
+
+        [Category("連桿結構"),Description("固定桿的長度")]
+        public double GroundLength
+        {
+            get => Lg;
+
+            set
+            {
+                double originalLength = Lg;
+                Lg = value;
+                if (!isQuadrilateralFeasible())
+                {
+                    Lg = originalLength;
+                    warnning = "Invalid Value";
+                }
+                else
+                    warnning = "";
+
+            }
+        }
+
+        [Category("連桿結構"),Description("驅動桿的長度")]
+        public double DriverLength
+        {
+            get => Ld;
+
+            set
+            {
+                double originalLength = Ld;
+                Ld = value;
+                if (!isQuadrilateralFeasible())
+                {
+                    Ld = originalLength;
+                    warnning = "Invalid Value";
+                }
+                else
+                    warnning = "";
+
+            }
+        }
+
+        [Category("連桿結構"),Description("連接桿的長度")]
+        public double ConnectorLength
+        {
+            get => Lc;
+
+            set
+            {
+                double originalLength = Lc;
+                Lc = value;
+                if (!isQuadrilateralFeasible())
+                {
+                    Lc = originalLength;
+                    warnning = "Invalid Value";
+                }
+                else
+                    warnning = "";
+
+            }
+        }
+
+        [Category("連桿結構"),Description("被動桿的長度")]
+        public double FollowerLength
+        {
+            get => Lf;
+
+            set
+            {
+                double originalLength = Lf;
+                Lf = value;
+                if (!isQuadrilateralFeasible())
+                {
+                    Lf = originalLength;
+                    warnning = "Invalid Value";
+                }
+                else
+                    warnning = "";
+
+            }
+        }
+
+
+        bool isQuadrilateralFeasible()
+        {
+            if (Lg > Ld + Lc + Lf || Ld > Lc + Lf + Lg || Lc > Lf + Lg + Ld || Lf > Lg + Ld + Lc)
+                return false;
+            return true;
+        }
+
+        //if the configuration of the linkage for newAlpha angle is
+        //feasible return true and update current alpha
+        public bool updateConfiguration(double newAlpha)
+        {
+            P2.X = (float)Lg;
+            P2.Y = 0.0f;
+            Pc.X = (float)(Ld * Math.Cos(newAlpha));
+            Pc.Y = (float)(Ld * Math.Sin(newAlpha));
+
+            float L;
+            L = (float)Math.Sqrt(Math.Pow((Lg - Pc.X), 2) + Math.Pow(Pc.Y, 2));
+            if (Lf + Lc < L || L + Lc < L || L + Lf < Lc)
+            {
+                this.warnning = "Invalid value";
+                return false;
+            }
+
+            alpha = newAlpha;
+
+            // Calculate Pf.X, Pf.Y
+            double omega;
+            double theta;
+
+            omega = Math.Acos((Math.Pow(Lf, 2) + Math.Pow(L, 2) - Math.Pow(Lc, 2)) / (2 * Lf * L));
+
+            // Use Math.Atan2(delta y,delta x)
+            theta = Math.Atan2(Pc.Y, (Pc.X - Lg)) - omega;
+
+            Pf.X = (float)(Lf * Math.Cos(theta) + Lg);
+            Pf.Y = (float)(Lf * Math.Sin(theta));
+
+            float unitX, unitY;
+
+            unitX = (float)((Pf.X - Pc.X) / Math.Sqrt(Math.Pow(Pf.X - Pc.X, 2) + Math.Pow(Pf.Y - Pc.Y, 2)));
+            unitY = (float)((Pf.Y - Pc.Y) / Math.Sqrt(Math.Pow(Pf.X - Pc.X, 2) + Math.Pow(Pf.Y - Pc.Y, 2)));
+
+            Pp.X = Pc.X + (float)(unitX * Da - unitY * Db);
+            Pp.Y = Pc.Y + (float)(unitY * Da + unitX * Db);
+
+            return true;
+        }
+
+    }
+}
---
+++ FourBarLinkage/MainForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R09725060HSHunagAss11
+{
+    public partial class MainForm : Form
+    {
+        FourBarLinkage myLinkage;
+        public MainForm()
+        {
+            InitializeComponent();
+            myLinkage = new FourBarLinkage();
+            //ppgLinkage.SelectedObject = myLinkage;
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            object[] pars = { ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true };
+            MethodInfo mm= typeof(Panel).GetMethod("SetStyle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            mm.Invoke(spcMain.Panel2, pars);
+        }
+
+        private void btnCalculation_Click(object sender, EventArgs e)
+        {
+            double delta = (double)nudDeltaAngle.Value;
+            double limit = Math.PI * 2;
+            rtbOutput.Clear();
+            for (double angle = 0.0; angle <= limit; angle += delta)
+            {
+                myLinkage.updateConfiguration(angle);
+                rtbOutput.AppendText(myLinkage.ToString());
+                rtbOutput.AppendText("\n");
+            }
+
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = button1.CreateGraphics();
+            Draw(g, button1.ClientRectangle);
+            g = spcMain.Panel2.CreateGraphics();
+            Draw(g, spcMain.Panel2.ClientRectangle);
+
+            Rectangle rect = new Rectangle(10, 5, 100, 50);
+            //rect.Width = 100;
+            //rect.Height = 50;
+            //rect.X = 10;
+            //rect.Y = 5;
+            g.FillEllipse(Brushes.Gold, rect);
+            g.DrawEllipse(Pens.Red, rect);
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(button1.Width, button1.Height);
+            Pen myPen = new Pen(Color.Blue, 10.0f);
+            g.DrawLine(myPen, p1, p2);
+            g.DrawString("NTU", this.Font, Brushes.Magenta, 0, 0);
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;//橫向的
+            sf.LineAlignment = StringAlignment.Center;
+            g.DrawString("機械系", this.Font,Brushes.Maroon,rect,sf);
+            Font myFont = new Font("微軟正黑體", 36);
+            g.DrawString("機械系", myFont, Brushes.Maroon, button1.ClientRectangle, sf);
+
+
+        }*/
+
+        /*void Draw(Graphics g, Rectangle bound)
+        {
+            Rectangle rect = new Rectangle(10, 5, 100, 50);
+            //rect.Width = 100;
+            //rect.Height = 50;
+            //rect.X = 10;
+            //rect.Y = 5;
+            g.FillEllipse(Brushes.Gold, rect);
+            g.DrawEllipse(Pens.Red, rect);
+            Point p1 = new Point(0, 0);
+            //Point p2 = new Point(bound.Width, button1.Height);
+            Pen myPen = new Pen(Color.Blue, 10.0f);
+            g.DrawLine(myPen, p1, p2);
+            g.DrawString("NTU", this.Font, Brushes.Magenta, 0, 0);
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;//橫向的
+            sf.LineAlignment = StringAlignment.Center;
+            g.DrawString("機械系", this.Font, Brushes.Maroon, rect, sf);
+            Font myFont = new Font("微軟正黑體", 36);
+            g.DrawString("機械系", myFont, Brushes.Maroon, bound, sf);
+        }*/
+
+        private void spcMain_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            //Draw(e.Graphics, e.ClipRectangle);
+            if(myLinkage!=null)
+            {
+                myLinkage.DrawLinkage(e.Graphics, e.ClipRectangle);
+                //draw all pois point
+                Rectangle r = new Rectangle(0, 0, 6, 6);
+                foreach(Point pt in POIs)
+                {
+                    r.X = pt.X - 3;
+                    r.Y = pt.Y - 3;
+                    e.Graphics.DrawRectangle(Pens.DarkGray, r);
+                }
+            }
+            labWarning.Text = myLinkage.Warnning;
+                
+        }
+
+        List<Point> POIs = new List<Point>();
+
+        float currentAngle = 0;
+        private void tmrAnimation_Tick(object sender, EventArgs e)
+        {
+            myLinkage.updateConfiguration(currentAngle);
+            if(chbtrajectory.Checked)
+                POIs.Add(myLinkage.PointOfInterest);
+
+            if (rdbFast.Checked)
+                currentAngle += 0.4f;
+            else if (rdbSlow.Checked)
+                currentAngle += 0.05f;
+            else
+                currentAngle += 0.2f;
+            spcMain.Panel2.Refresh();
+        }
+
+        private void btnAnimation_Click(object sender, EventArgs e)
+        {
+            tmrAnimation.Enabled = !tmrAnimation.Enabled;
+            if (tmrAnimation.Enabled)
+                btnAnimation.Text = "Stop";
+            else
+                btnAnimation.Text = "Animate";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            POIs.Clear();
+            spcMain.Panel2.Refresh();
+        }
+
+        private void tbGroundLength_Scroll(object sender, EventArgs e)
+        {
+            labGoundLength.Text = tbGroundLength.Value.ToString();
+            myLinkage.GroundLength = tbGroundLength.Value;
+            tbGroundLength.Value = (int)myLinkage.GroundLength;
+            labGoundLength.Text = tbGroundLength.Value.ToString();
+        }
+
+        private void tbConnectorLength_Scroll(object sender, EventArgs e)
+        {
+            labConnectorLength.Text = tbConnectorLength.Value.ToString();
+            myLinkage.ConnectorLength = tbConnectorLength.Value;
+            tbConnectorLength.Value = (int)myLinkage.ConnectorLength;
+            labConnectorLength.Text = tbConnectorLength.Value.ToString();
+            spcMain.Panel2.Refresh();
+            //labWarning.Text = myLinkage.Warnning;
+        }
+
+        private void tbDriverLength_Scroll(object sender, EventArgs e)
+        {
+            labDriverLength.Text = tbDriverLength.Value.ToString();
+            myLinkage.DriverLength = tbDriverLength.Value;
+            tbDriverLength.Value = (int)myLinkage.DriverLength;
+            labDriverLength.Text = tbDriverLength.Value.ToString();
+            spcMain.Panel2.Refresh();
+            labWarning.Text = myLinkage.Warnning;
+        }
+
+        private void tbFollwerLength_Scroll(object sender, EventArgs e)
+        {
+            labFollowerLength.Text = tbFollowerLength.Value.ToString();
+            myLinkage.FollowerLength = tbFollowerLength.Value;
+            tbFollowerLength.Value = (int)myLinkage.FollowerLength;
+            labFollowerLength.Text = tbFollowerLength.Value.ToString();
+            spcMain.Panel2.Refresh();
+            labWarning.Text = myLinkage.Warnning;
+        }
+
+        private void tbPOIAxialOffsets_Scroll(object sender, EventArgs e)
+        {
+            labPOIAxialOffsets.Text = tbPOIAxialOffsets.Value.ToString();
+            myLinkage.POIAxialOffset = tbPOIAxialOffsets.Value;
+            tbPOIAxialOffsets.Value = (int)myLinkage.POIAxialOffset;
+            labPOIAxialOffsets.Text = tbPOIAxialOffsets.Value.ToString();
+            labWarning.Text = myLinkage.Warnning;
+        }
+
+        private void tbPOIRaiaOffsets_Scroll(object sender, EventArgs e)
+        {
+            labPOIRadialOffsets.Text = tbPOIRaialOffsets.Value.ToString();
+            myLinkage.POIRadialOffset = tbPOIRaialOffsets.Value;
+            tbPOIRaialOffsets.Value = (int)myLinkage.POIRadialOffset;
+            labPOIRadialOffsets.Text = tbPOIRaialOffsets.Value.ToString();
+            spcMain.Panel2.Refresh();
+            labWarning.Text = myLinkage.Warnning;
+        }
+    }
+}
